069263fec7ac2a6f66478579ed2dabc7abcc6a2a
kiran kumar reddy ramireddy <ext_kkreddy@falabella.cl>
2020-05-10 01:36:34 +0530

    remove client image build and test from process

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-docker build -t kiranvits/multi-client:latest -t kiranvits/multi-client:$SHA -f ./client/Docketfile ./client
+# docker build -t kiranvits/multi-client:latest -t kiranvits/multi-client:$SHA -f ./client/Docketfile ./client
 docker build -t kiranvits/multi-server:latest -t kiranvits/multi-server:$SHA -f ./server/Docketfile ./server
 docker build -t kiranvits/multi-worker:latest -t kiranvits/multi-worker:$SHA -f ./worker/Docketfile ./worker
 # push latest images
-docker push kiranvits/multi-client:latest
+# docker push kiranvits/multi-client:latest
 docker push kiranvits/multi-server:latest
 docker push kiranvits/multi-worker:latest
 # push release images
-docker push kiranvits/multi-client:$SHA
+# docker push kiranvits/multi-client:$SHA
 docker push kiranvits/multi-server:$SHA
 docker push kiranvits/multi-worker:$SHA
 # apply all configurations
